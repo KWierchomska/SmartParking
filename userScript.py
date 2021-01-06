e0b75c6b26320d1675e9bd0adddc8e6022444ad9
Izabela Czajowska <izabelaczajowska@gmail.com>
2021-01-06 20:46:12 +0100

    display results on the map

--- userScript.py
+++ userScript.py
@@ -1,9 +1,27 @@
 import requests
 import geopy.distance
 import time
+import matplotlib.pyplot as plt
 
 URL = "https://8oweaskc5f.execute-api.us-east-1.amazonaws.com/test/queries"
 params = {"lat":  0, "long": 0, "radius": 100}
+
+ruh_m = plt.imread('./high_resolution_image.png')
+
+MAP_LONG_MIN = 19.918226
+MAP_LONG_MAX = 19.973078
+MAP_LAT_MIN = 50.050085
+MAP_LAT_MAX = 50.085449
+
+# Boundary Box of the map file high_resolution_image.png
+BBox = (MAP_LONG_MIN, MAP_LONG_MAX, MAP_LAT_MIN, MAP_LAT_MAX)
+
+
+# Example latitude, longitude arrays
+LATITUDE_ARR = [50.06477478214624, 50.062690310916075, 50.05767280893131, 50.05571219152061, 50.05862094762662]
+LONGITUDE_ARR = [19.93192798675469, 19.931092082082436, 19.9307177912443, 19.942482360328277, 19.945385956102193]
+# Point color array
+COLORS = ['b' for _ in LONGITUDE_ARR]
 
 while True:
     print("Enter latitude:")
@@ -12,10 +30,15 @@ while True:
     long = input()
     print("Enter radius of search:")
     radius = input()
+    # Example values: 50.066178137497, 19.936686213350587
 
     params["lat"] = float(lat)
     params["long"] = float(long)
     params["radius"] = float(radius)
+
+    LATITUDE_ARR.append(float(lat))
+    LONGITUDE_ARR.append(float(long))
+    COLORS.append('r')
 
     response = requests.get(URL, params).json()
 
@@ -26,4 +49,22 @@ while True:
         dist = geopy.distance.distance(c1,c2).km
         print(str(it) + " latitude,longitude: " + el["geoJson"]["S"] + "; distance: " + str(dist))
 
+        # TODO: Add latitude, longitude to arrays + set color of new point
+
+    # DRAW PLOT
+    fig, ax = plt.subplots(figsize=(10, 10))
+    ax.scatter(LONGITUDE_ARR, LATITUDE_ARR, zorder=1, alpha=0.2, c=COLORS, s=150)
+
+    ax.set_title('Available Parking Spots')
+
+    ax.set_xlim(BBox[0], BBox[1])
+    ax.set_ylim(BBox[2], BBox[3])
+    # ax.set_aspect('equal', 'box')
+
+    ax.imshow(ruh_m, zorder=0, extent=BBox, aspect='equal')
+
+    # Zoom area where parking spots are
+    ax.set_xlim(min(LONGITUDE_ARR) - 0.005, max(LONGITUDE_ARR) + 0.005)
+    ax.set_ylim(min(LATITUDE_ARR) - 0.005, max(LATITUDE_ARR) + 0.005)
+    plt.show()
     #time.sleep(10)
