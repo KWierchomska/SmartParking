761b4f575f928b2d8b21a3eff4cb0eae4c90556b
FluffyNinjaBrick <1111do.gier1111@gmail.com>
2021-01-07 20:09:05 +0100

    added email low battery notification; also slight refactoring

--- Sensors/sensor10.py
+++ Sensors/sensor10.py
@@ -4,8 +4,14 @@ import time
 import config
 
 SHADOW_CLIENT = "Sensor10"
+MQTT_CLIENT = "Sensor10_MQTT"
 
-shadow = config.setup(SHADOW_CLIENT)
+shadow = config.setupShadow(SHADOW_CLIENT)
+mqttClient = config.setupMQTT(MQTT_CLIENT)
+
+battery = 1.0
+batteryWarningSent = False
+
 while True:
     available = random.choice([True, False])
     data = {"state": {
@@ -15,10 +21,23 @@ while True:
             "longitude": 19.941958,
             "available": available,
             "time": str(datetime.datetime.now()),
-            "battery": 0.7,
+            "battery": battery,
             "address": "Szpitalna 30, Kraków",
             "payable": False
         }
     }}
-    config.send(data, shadow)
+
+    battery = config.sendShadow(data, shadow)
+
+    if battery <= 0.2 and not batteryWarningSent:
+        config.sendMQTT(mqttClient, "SENSOR10", battery)
+        batteryWarningSent = True
+    if battery > 0.2:
+        batteryWarningSent = False
+
+    if battery == 0.0:
+        shadow.disconnect()
+        mqttClient.disconnect()
+        exit(1)
+
     time.sleep(60)
--- Sensors/sensor9.py
+++ Sensors/sensor9.py
@@ -4,8 +4,14 @@ import time
 import config
 
 SHADOW_CLIENT = "Sensor9"
+MQTT_CLIENT = "Sensor9_MQTT"
 
-shadow = config.setup(SHADOW_CLIENT)
+shadow = config.setupShadow(SHADOW_CLIENT)
+mqttClient = config.setupMQTT(MQTT_CLIENT)
+
+battery = 1.0
+batteryWarningSent = False
+
 while True:
     available = random.choice([True, False])
     data = {"state": {
@@ -15,10 +21,23 @@ while True:
             "longitude": 19.943154,
             "available": available,
             "time": str(datetime.datetime.now()),
-            "battery": 0.7,
+            "battery": battery,
             "address": "Plac Swiętego Ducha 1, Kraków",
             "payable": True
         }
     }}
-    config.send(data, shadow)
+
+    battery = config.sendShadow(data, shadow)
+
+    if battery <= 0.2 and not batteryWarningSent:
+        config.sendMQTT(mqttClient, "SENSOR9", battery)
+        batteryWarningSent = True
+    if battery > 0.2:
+        batteryWarningSent = False
+
+    if battery == 0.0:
+        shadow.disconnect()
+        mqttClient.disconnect()
+        exit(1)
+
     time.sleep(60)
